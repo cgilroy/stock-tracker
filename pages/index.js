c81009779b63f90c850835add576827c2ceec18e
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-03-18 19:51:17 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import App from '../components/App.js'
+import Head from 'next/head'
+export default () => (
+  <div>
+  <Head>
+  <title>Hockey Cards</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charSet="utf-8" />
+</Head>
+  <App />
+  </div>
+)
