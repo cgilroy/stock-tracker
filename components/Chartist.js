e661c97aa086c4ca6db4b6eb08baf3517cf97f9d
CJ Gilroy <c.gilroy9@gmail.com>
2019-04-16 14:59:01 -0600

    Work on adding tracking line to chart tooltip

--- components/Chartist.js
+++ components/Chartist.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ChartistGraph from 'react-chartist';
 import './chartist.scss'
+import './chartist.css'
 import { formatMoney } from './helpers.js'
 const Chart = (props) => {
 
