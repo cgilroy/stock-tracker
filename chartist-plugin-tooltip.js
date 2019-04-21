28a7b2acf61966e4ee39c0996b82e655f00084ff
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-04-21 12:33:00 -0600

    Fixing tooltip movement and clean up

--- chartist-plugin-tooltip.js
+++ chartist-plugin-tooltip.js
@@ -45,13 +45,6 @@
                 .append('<div class="ct-tooltip"></div>')
                 .find('.ct-tooltip')
                 .hide();
-            // var $tooltipLine = $chart
-            // .append('<div class="ct-tooltip-line"></div>')
-            // .find('.ct-tooltip-line');
-            // $tooltipLine.css({
-            //   height: '100px',
-            //   width: '30px'
-            // });
             $chart.on('mouseenter', tooltipSelector, function() {
                 var $point = $(this),
                     seriesName = $point.parent().attr('ct:series-name'),
@@ -82,12 +75,8 @@
                     left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
                     top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height() - 40
                 });
-                // $('.ct-tooltip-line').css({
-                //   left: (event.offsetX+10),
-                //   top: 0
-                // })
             });
-            $chart.on('mousemove mouseenter mouseleave', '.ct-tooltip-line', function(event) {
+            $chart.on('mousemove mouseenter mouseleave', '.ct-tooltip-line-x', function(event) {
               event.stopPropagation();
               event.preventDefault();
 
@@ -96,14 +85,42 @@
             });
 
             $chart.on('mousemove', '.ct-grid-background', function(event) {
-              $chart.find('.ct-tooltip-line').attr('x1', event.offsetX)
-              $chart.find('.ct-tooltip-line').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x1', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-x').attr('y1', event.offsetY)
+              $chart.find('.ct-tooltip-line-x').attr('y2', event.offsetY)
+            });
+            $chart.on('mousemove', '.ct-tooltip-line-x', function(event) {
+              $chart.find('.ct-tooltip-line-y').attr('x1', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-x').attr('y1', event.offsetY)
+              $chart.find('.ct-tooltip-line-x').attr('y2', event.offsetY)
+            });
+            $chart.on('mousemove', '.ct-tooltip-line-y', function(event) {
+              $chart.find('.ct-tooltip-line-y').attr('x1', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-x').attr('y1', event.offsetY)
+              $chart.find('.ct-tooltip-line-x').attr('y2', event.offsetY)
+            });
+            $chart.on('mousemove', '.ct-line', function(event) {
+              $chart.find('.ct-tooltip-line-y').attr('x1', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-x').attr('y1', event.offsetY)
+              $chart.find('.ct-tooltip-line-x').attr('y2', event.offsetY)
+            });
+            $chart.on('mousemove', '.ct-point', function(event) {
+              $chart.find('.ct-tooltip-line-y').attr('x1', event.offsetX)
+              $chart.find('.ct-tooltip-line-y').attr('x2', event.offsetX)
+              $chart.find('.ct-tooltip-line-x').attr('y1', event.offsetY)
+              $chart.find('.ct-tooltip-line-x').attr('y2', event.offsetY)
             });
             $chart.on('mouseenter', '.ct-grid-background', function(event) {
-              $chart.find('.ct-tooltip-line').show()
+              $chart.find('.ct-tooltip-line-y').show()
+              $chart.find('.ct-tooltip-line-x').show()
             });
             $chart.on('mouseleave', function(event) {
-              $chart.find('.ct-tooltip-line').hide()
+              $chart.find('.ct-tooltip-line-y').hide()
+              $chart.find('.ct-tooltip-line-x').hide()
             });
 
         };
